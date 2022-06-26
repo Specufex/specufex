dbe0e238784aeae41212a77085d0e5c052ab186b
Nate Groebner <groe0029@umn.edu>
2022-06-26 10:30:55 -0500

    setup nox testing

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,7 @@
 # noxfile.py
 import nox
-@nox.session(python=["3.10", "3.9", "3.8"])
+@nox.session(python=["3.10", "3.9", "3.8"], venv_backend="mamba")
 def tests(session):
-    session.run("pytest", "--cov")
+    session.run("mamba", "install", "-y", "--file", "requirements.txt")
+    session.install(".", "--no-deps")
+    session.run("pytest")
