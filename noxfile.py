f43e0b743c60d5b744a813bf995077775edd6982
Nate Groebner <groe0029@umn.edu>
2022-06-26 17:36:23 -0500

    lint, flake8, black successful

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,25 @@
 # noxfile.py
 import nox
+
+locations = "specufex", "tests", "noxfile.py"
+
+
 @nox.session(python=["3.10", "3.9", "3.8"], venv_backend="mamba")
 def tests(session):
     session.run("mamba", "install", "-y", "--file", "requirements.txt")
     session.install(".", "--no-deps")
     session.run("pytest")
+
+
+@nox.session(python=["3.10", "3.9", "3.8"], venv_backend="mamba")
+def lint(session):
+    args = session.posargs or locations
+    session.install("flake8")
+    session.run("flake8", *args)
+
+
+@nox.session(python=["3.10", "3.9", "3.8"], venv_backend="mamba")
+def black(session):
+    args = session.posargs or locations
+    session.install("black")
+    session.run("black", *args)
--- specufex/__init__.py
+++ specufex/__init__.py
@@ -4,3 +4,4 @@ from .nmf import BayesianNonparametricNMF
 # from .utilities import normalize_spectrogram
 
 __version__ = "0.1.0"
+__all__ = ["BayesianHMM", "BayesianNonparametricNMF"]
