c0acad72f5faf884d1f6f335377cbfcdac1c7ba6
Nate Groebner <groe0029@umn.edu>
2022-06-26 09:55:35 -0500

    coverage

--- specufex/utilities.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-
-def normalize_spectrogram(X):
-    """Apply normalization from Holtzman et al. 2018 to a spectrogram."""
-    return np.maximum(0, np.floor(20 * np.log10(X / np.median(X))))
