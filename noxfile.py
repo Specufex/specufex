a96b0856cd407416d940eed7b280501f518ae9d2
Nate Groebner <groe0029@umn.edu>
2022-06-27 16:52:23 -0500

    change flake8 conf to pyproj.toml

--- noxfile.py
+++ noxfile.py
@@ -12,10 +12,17 @@ def tests(session):
     session.run("pytest")
 
 
-@nox.session(python=["3.10", "3.9", "3.8"], venv_backend="mamba")
+"""@nox.session(python=["3.10", "3.9", "3.8"], venv_backend="mamba")
 def lint(session):
     args = session.posargs or locations
     session.install("flake8", "flake8-black")  # flake8-black checks for black changes
+    session.run("flake8", *args)"""
+
+
+@nox.session(python=["3.10", "3.9", "3.8"], venv_backend="mamba")
+def lint(session):
+    args = session.posargs or locations
+    session.install("flake8", "flake8-black", "flake8-isort", "flake8-pyprojecttoml")
     session.run("flake8", *args)
 
 
