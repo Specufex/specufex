0a9193c64761996cce8473587c6a1cac41dfc3d0
Nate Groebner <groe0029@umn.edu>
2022-06-27 21:08:43 -0500

    add github action for ci

--- noxfile.py
+++ noxfile.py
@@ -5,7 +5,7 @@ locations = "specufex", "tests", "noxfile.py"
 nox.options.sessions = "lint", "tests"
 
 
-@nox.session(python=["3.10", "3.9", "3.8", "3.7"], venv_backend="mamba")
+@nox.session(python=["3.10", "3.9", "3.8"], venv_backend="mamba")
 def tests(session):
     session.run("mamba", "install", "-y", "--file", "requirements.txt")
     session.install(".", "--no-deps")
