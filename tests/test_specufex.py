2f095985fa9c0b0d6923ca0b2e96f7ebdcb34627
Nate Groebner <groe0029@umn.edu>
2024-03-23 18:32:15 -0500

    updated tests

--- tests/test_specufex.py
+++ tests/test_specufex.py
@@ -1,222 +1,187 @@
 import os
-from pathlib import Path
-
-import h5py
+import pytest
 import numpy as np
 
 from specufex import BayesianHMM, BayesianNonparametricNMF
 
+@pytest.fixture
+def setup_nmf():
+    # for the NMF tests
+    N, D, M = 10, 5, 7
+    X = np.random.poisson(lam=1.0, size=(N, D, M))
+    nmf = BayesianNonparametricNMF(X.shape)
+    if os.path.exists("tests/tested_save_nmf.h5"):
+        os.remove("tests/tested_save_nmf.h5")
+    return [X, nmf]
 
-class TestNMF:
-    def setup(self):
-
-        # for the NMF tests
-        N, D, M = 10, 5, 7
-        self.X = np.random.poisson(lam=1.0, size=(N, D, M))
-        self.nmf = BayesianNonparametricNMF(self.X.shape)
-        if os.path.exists("tests/tested_save_nmf.h5"):
-            os.remove("tests/tested_save_nmf.h5")
-
-    def teardown(self):
-        if os.path.exists("tests/tested_save_nmf.h5"):
-            os.remove("tests/tested_save_nmf.h5")
-
-    def test_fit(self):
-        """Test the NMF fit method. Checks that the calculated EW and EA
-        matrices when multiplied have same first dimension as X.
-        """
-
-        # default verbosity
-        self.nmf.fit(self.X)
-        fit_shape = self.nmf.EW @ np.diag(self.nmf.EA[0])
-        assert self.X.shape[1] == fit_shape.shape[0]
-
-        # verbosity 1
-        self.nmf.fit(self.X, verbose=1)
-        fit_shape = self.nmf.EW @ np.diag(self.nmf.EA[0])
-        assert self.X.shape[1] == fit_shape.shape[0]
-
-        # verbosity > 1
-        self.nmf.fit(self.X, verbose=46)
-        fit_shape = self.nmf.EW @ np.diag(self.nmf.EA[0])
-        assert self.X.shape[1] == fit_shape.shape[0]
-
-    def test_fit_no_resort_args(self):
-        """Test the NMF fit method without resorting the As. Checks that
-        the calculated EW and EA matrices when multiplied have same first
-        dimension as X.
-        """
-        self.nmf.fit(self.X, resort_args=False)
-        fit_shape = self.nmf.EW @ np.diag(self.nmf.EA[0])
-
-        assert self.X.shape[1] == fit_shape.shape[0]
-
-    def test_fit_stepsize(self):
-        """Test the NMF fit method when stepsize is specified.
-        Checks that the calculated EW and EA matrices when multiplied
-        have same first dimension as X.
-        """
-        self.nmf.fit(self.X, stepsize=0.01)
-        fit_shape = self.nmf.EW @ np.diag(self.nmf.EA[0])
-
-        assert self.X.shape[1] == fit_shape.shape[0]
-
-    def test_transform(self):
-        """Test the NMF transform method. Checks that when a transformed
-        V matrix is multiplied by EWA it has same dimension as
-        X
-        """
-        self.nmf.fit(self.X)
-        Vs = self.nmf.transform(self.X)
-        est_X = self.nmf.EW @ np.diag(self.nmf.EA[0]) @ Vs[0]
-        assert self.X[0].shape == est_X.shape
-
-    def test_fit_transform(self):
-        """Test the NMF transform method. Checks that when a transformed
-        V matrix is multiplied by EWA it has same dimension as
-        X
-        """
-
-        # default verbosity
-        Vs = self.nmf.fit_transform(self.X)
-        est_X = self.nmf.EW @ np.diag(self.nmf.EA[0]) @ Vs[0]
-        assert self.X[0].shape == est_X.shape
-
-        # verbosity > 0
-        Vs = self.nmf.fit_transform(self.X, verbose=1)
-        est_X = self.nmf.EW @ np.diag(self.nmf.EA[0]) @ Vs[0]
-        assert self.X[0].shape == est_X.shape
-
-    def test_save(self):
-        """test NMF model save"""
-        self.nmf.fit(self.X)
-        assert self.nmf.save("tests/tested_save_nmf.h5", overwrite=False)
+def teardown():
+    if os.path.exists("tests/tested_save_nmf.h5"):
         os.remove("tests/tested_save_nmf.h5")
 
-    def test_overwrite_save(self):
-        """test NMF model save when file is present"""
-        self.nmf.fit(self.X)
-        Path("tests/tested_save_nmf.h5").touch(exist_ok=False)
-        assert not self.nmf.save("tests/tested_save_nmf.h5", overwrite=False)
-        os.remove("tests/tested_save_nmf.h5")
+def test_nmf_fit(setup_nmf):
+    """Test the NMF fit method. Checks that the calculated EW and EA
+    matrices when multiplied have same first dimension as X.
+    """
 
-    def test_load(self):
-        """Test model loading"""
-        nmf = BayesianNonparametricNMF.load("tests/test_nmf_params.h5")
-        assert isinstance(nmf, BayesianNonparametricNMF)
+    X, nmf = setup_nmf
+    # default verbosity
+    nmf.fit(X)
+    fit_shape = nmf.EW @ np.diag(nmf.EA[0])
+    assert X.shape[1] == fit_shape.shape[0]
 
-        with h5py.File("tests/test_nmf_params.h5") as hf:
-            assert (nmf.EW == hf["EW"][()]).all()
-            assert (nmf.EA == hf["EA"][()]).all()
-            # might want to add tests for all the parameters - do the member thing
+    # verbosity 1
+    nmf.fit(X, verbose=1)
+    fit_shape = nmf.EW @ np.diag(nmf.EA[0])
+    assert X.shape[1] == fit_shape.shape[0]
 
+    # verbosity > 1
+    nmf.fit(X, verbose=46)
+    fit_shape = nmf.EW @ np.diag(nmf.EA[0])
+    assert X.shape[1] == fit_shape.shape[0]
 
-class TestHMM:
-    def setup(self):
-        """set up"""
-        N, num_pat, timesteps = 10, 5, 7
-        self.V = np.random.poisson(lam=1.0, size=(N, num_pat, timesteps))
-        gain = np.ones(num_pat)
+def test_fit_no_resort_args(setup_nmf):
+    """Test the NMF fit method without resorting the As. Checks that
+    the calculated EW and EA matrices when multiplied have same first
+    dimension as X.
+    """
+    X, nmf = setup_nmf
 
-        self.hmm = BayesianHMM(num_pat, gain)
+    nmf.fit(X, resort_args=False)
+    fit_shape = nmf.EW @ np.diag(nmf.EA[0])
 
-        if os.path.exists("tests/tested_save_hmm.h5"):
-            os.remove("tests/tested_save_hmm.h5")
+    assert X.shape[1] == fit_shape.shape[0]
 
-    def teardown(self):
-        if os.path.exists("tests/tested_save_hmm.h5"):
-            os.remove("tests/tested_save_hmm.h5")
+def test_fit_stepsize(setup_nmf):
+    """Test the NMF fit method when stepsize is specified.
+    Checks that the calculated EW and EA matrices when multiplied
+    have same first dimension as X.
+    """
+    X, nmf = setup_nmf
 
-    def test_fit_default(self):
-        """test the HMM fit method"""
+    nmf.fit(X, stepsize=0.01)
+    fit_shape = nmf.EW @ np.diag(nmf.EA[0])
 
-        # default verbosity
-        self.hmm.fit(self.V)
-        assert self.hmm.EB.shape == (self.hmm.num_state, self.hmm.num_pat)
+    assert X.shape[1] == fit_shape.shape[0]
 
-        # verbosity > 0
-        self.hmm.fit(self.V, verbose=23)
-        assert self.hmm.EB.shape == (self.hmm.num_state, self.hmm.num_pat)
+def test_transform(setup_nmf):
+    """Test the NMF transform method. Checks that when a transformed
+    V matrix is multiplied by EWA it has same dimension as
+    X
+    """
+    X, nmf = setup_nmf
+    nmf.fit(X)
+    Vs = nmf.transform(X)
+    est_X = nmf.EW @ np.diag(nmf.EA[0]) @ Vs[0]
+    assert X[0].shape == est_X.shape
 
-    def test_fit_EB_dist_sort(self):
-        """Test that EB is sorted by pairwise distances.
-        Currently only checks that the shape is correct as above.
-        """
-        self.hmm.fit(self.V, resort_EB="distance")
+def test_fit_transform(setup_nmf):
+    """Test the NMF transform method. Checks that when a transformed
+    V matrix is multiplied by EWA it has same dimension as
+    X
+    """
+    X, nmf = setup_nmf
+    # default verbosity
+    Vs = nmf.fit_transform(X)
+    est_X = nmf.EW @ np.diag(nmf.EA[0]) @ Vs[0]
+    assert X[0].shape == est_X.shape
 
-        assert self.hmm.EB.shape == (self.hmm.num_state, self.hmm.num_pat)
+    # verbosity > 0
+    Vs = nmf.fit_transform(X, verbose=1)
+    est_X = nmf.EW @ np.diag(nmf.EA[0]) @ Vs[0]
+    assert X[0].shape == est_X.shape
 
-    def test_fit_EB_energy_sort(self):
-        """Test that EB is sorted by decreasing energy, i.e., gradient is nonpositive"""
-        self.hmm.fit(self.V, resort_EB="energy")
-        energies = self.hmm.EB.sum(axis=1)
-        energies_diff = np.diff(energies)
-        assert np.all(energies_diff <= 0)
+@pytest.fixture
+def setup_hmm():
+    """set up"""
+    N, num_pat, timesteps = 10, 5, 7
+    V = np.random.poisson(lam=1.0, size=(N, num_pat, timesteps))
+    gain = np.ones(num_pat)
 
-    def test_getStateMatrices(self):
-        """Test the _getStateMatrices method. Only checks to see
-        if matrix dimensions are correct.
-        """
-        self.hmm.fit(self.V, resort_EB="energy")
-        As, ppis, gams = self.hmm._getStateMatrices(self.V)
-        assert As.shape == (self.V.shape[0], self.hmm.num_state, self.hmm.num_state)
-        assert ppis.shape == (self.V.shape[0], self.hmm.num_state)
-        assert gams.shape == (self.V.shape[0], self.hmm.num_state, self.V.shape[2])
+    hmm = BayesianHMM(num_pat, gain)
 
-    def test_getFingerprints(self):
-        """Test the _getFingerprints method"""
-        self.hmm.fit(self.V, resort_EB="energy")
-        As, ppis, _ = self.hmm._getStateMatrices(self.V)
-        fprints = self.hmm._getFingerprints(As, ppis)
-        assert fprints.shape == (
-            self.V.shape[0],
-            self.hmm.num_state,
-            self.hmm.num_state,
-        )
-
-    def test_transform(self):
-        """test the HMM transform method"""
-        self.hmm.fit(self.V, resort_EB="energy")
-        fprints, As, gams = self.hmm.transform(self.V)
-        assert fprints.shape == (
-            self.V.shape[0],
-            self.hmm.num_state,
-            self.hmm.num_state,
-        )
-        assert As.shape == (self.V.shape[0], self.hmm.num_state, self.hmm.num_state)
-        assert gams.shape == (self.V.shape[0], self.hmm.num_state, self.V.shape[2])
-
-    def test_fit_transform(self):
-        """Test fit_transform method"""
-        fprints, As, gams = self.hmm.fit_transform(self.V)
-        assert fprints.shape == (
-            self.V.shape[0],
-            self.hmm.num_state,
-            self.hmm.num_state,
-        )
-        assert As.shape == (self.V.shape[0], self.hmm.num_state, self.hmm.num_state)
-        assert gams.shape == (self.V.shape[0], self.hmm.num_state, self.V.shape[2])
-
-    def test_save(self):
-        """test HMM model save"""
-        self.hmm.fit(self.V)
-        assert self.hmm.save("tests/tested_save_hmm.h5", overwrite=False)
-        os.remove("tests/tested_save_hmm.h5")
-        # assert os.path.exists("tests/tested_save_hmm.h5")
-
-    def test_overwrite_save(self):
-        """test HMM model save when file is present"""
-        self.hmm.fit(self.V)
-        Path("tests/tested_save_hmm.h5").touch(exist_ok=False)
-        assert not self.hmm.save("tests/tested_save_hmm.h5", overwrite=False)
+    if os.path.exists("tests/tested_save_hmm.h5"):
         os.remove("tests/tested_save_hmm.h5")
 
-    def test_load(self):
-        """test HMM model loading"""
-        hmm = BayesianHMM.load("tests/test_hmm_params.h5")
-        assert isinstance(hmm, BayesianHMM)
+    return [V, hmm]
 
-        with h5py.File("tests/test_hmm_params.h5") as hf:
-            assert (hmm.EB == hf["EB"][()]).all()
-            # self.assertTrue( (hmm.EA==hf['EA'][()]).all() )
-            # might want to add tests for all the parameters - do the member thing
+def teardown(self):
+    if os.path.exists("tests/tested_save_hmm.h5"):
+        os.remove("tests/tested_save_hmm.h5")
+
+
+def test_hmm_fit_default(setup_hmm):
+    """test the HMM fit method"""
+
+    V, hmm = setup_hmm
+    # default verbosity
+    hmm.fit(V)
+    assert hmm.EB.shape == (hmm.num_state, hmm.num_pat)
+
+    # verbosity > 0
+    hmm.fit(V, verbose=23)
+    assert hmm.EB.shape == (hmm.num_state, hmm.num_pat)
+
+def test_fit_EB_dist_sort(setup_hmm):
+    """Test that EB is sorted by pairwise distances.
+    Currently only checks that the shape is correct as above.
+    """
+    V, hmm = setup_hmm
+    hmm.fit(V, resort_EB="distance")
+
+    assert hmm.EB.shape == (hmm.num_state, hmm.num_pat)
+
+def test_fit_EB_energy_sort(setup_hmm):
+    """Test that EB is sorted by decreasing energy, i.e., gradient is nonpositive"""
+    V, hmm = setup_hmm
+    hmm.fit(V, resort_EB="energy")
+    energies = hmm.EB.sum(axis=1)
+    energies_diff = np.diff(energies)
+    assert np.all(energies_diff <= 0)
+
+def test_getStateMatrices(setup_hmm):
+    """Test the _getStateMatrices method. Only checks to see
+    if matrix dimensions are correct.
+    """
+    V, hmm = setup_hmm
+    hmm.fit(V, resort_EB="energy")
+    As, ppis, gams = hmm._getStateMatrices(V)
+    assert As.shape == (V.shape[0], hmm.num_state, hmm.num_state)
+    assert ppis.shape == (V.shape[0], hmm.num_state)
+    assert gams.shape == (V.shape[0], hmm.num_state, V.shape[2])
+
+def test_getFingerprints(setup_hmm):
+    """Test the _getFingerprints method"""
+    V, hmm = setup_hmm
+    hmm.fit(V, resort_EB="energy")
+    As, ppis, _ = hmm._getStateMatrices(V)
+    fprints = hmm._getFingerprints(As, ppis)
+    assert fprints.shape == (
+        V.shape[0],
+        hmm.num_state,
+        hmm.num_state,
+    )
+
+def test_transform(setup_hmm):
+    """test the HMM transform method"""
+    V, hmm = setup_hmm
+    hmm.fit(V, resort_EB="energy")
+    fprints, As, gams = hmm.transform(V)
+    assert fprints.shape == (
+        V.shape[0],
+        hmm.num_state,
+        hmm.num_state,
+    )
+    assert As.shape == (V.shape[0], hmm.num_state, hmm.num_state)
+    assert gams.shape == (V.shape[0], hmm.num_state, V.shape[2])
+
+def test_fit_transform(setup_hmm):
+    """Test fit_transform method"""
+    V, hmm = setup_hmm
+    fprints, As, gams = hmm.fit_transform(V)
+    assert fprints.shape == (
+        V.shape[0],
+        hmm.num_state,
+        hmm.num_state,
+    )
+    assert As.shape == (V.shape[0], hmm.num_state, hmm.num_state)
+    assert gams.shape == (V.shape[0], hmm.num_state, V.shape[2])
